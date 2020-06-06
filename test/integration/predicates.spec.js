1185f305bd7b0bd195f7436690ee02e415e3bbfa
Alex Anderson <alxndrsn>
2020-06-06 09:35:41 +0000

    Add Martijn's predicate tests, and separate them

---
+++ test/integration/predicates.spec.js
@@ -0,0 +1,72 @@
+const { assertNumberValue, assertStringValue, assertTrue, initDoc } = require('../helpers');
+
+describe('predicates with function calls', ()=> {
+
+  describe('with native functions', () => {
+    [
+      [ 'count(/data/item[true()]) = 2',   assertTrue ],
+      [ 'count(/data/b[round(2.44) = 2])', assertNumberValue, 2 ],
+      [ '/data/item[true()]/number',       assertNumberValue, 4 ],
+      [ '/data/item[true()]/number + 1',   assertNumberValue, 5 ],
+      [ '/data/item[true()]/number + 1',   assertStringValue, '5' ],
+      [ '/data/item[string-length("a") = 1]/number + 2',    assertNumberValue, 6 ],
+      [ '/data/item[string-length(./number)=1]/number + 3', assertNumberValue, 7 ],
+      [ '/data/item[(./number div 3.14) > 1.9]/number',     assertNumberValue, 6 ],
+    ].forEach(([ expr, assertion, ...extraArgs ]) => {
+      it(`should evaluate ${expr} as expected`, () => {
+        initDoc(`
+          <data>
+            <item>
+            <number>4</number>
+            </item>
+            <item>
+            <number>6</number>
+            </item>
+            <b/>
+            <b/>
+          </data>
+        `);
+
+        assertion(expr, ...extraArgs);
+      });
+    });
+  });
+
+
+  describe('with extended functions', () => {
+    [
+      [ '/data/item[pi() > 3]/number',                  assertNumberValue, 4 ],
+      [ '/data/item[tan(./number) > 1]/number',         assertNumberValue, 4 ],
+      [ '/data/item[(./number div pi()) > 1.9]/number', assertNumberValue, 6 ],
+    ].forEach(([ expr, assertion, ...extraArgs ]) => {
+      it(`should evaluate ${expr} as expected`, () => {
+        initDoc(`
+          <data>
+            <item>
+            <number>4</number>
+            </item>
+            <item>
+            <number>6</number>
+            </item>
+          </data>
+        `);
+
+        assertion(expr, ...extraArgs);
+      });
+    });
+  });
+
+  // I put this one separate as it has a different 'too many args' error, and there may be multiple causes for failure
+  it('with the #selected function', () => {
+    initDoc(`
+      <data>
+      <a>a</a>
+      <a>b</a>
+      <a>c</a>
+      </data>
+    `);
+
+    // assertTrue('selected("a b", "a")');
+    assertNumberValue('count(/data/a[selected("a b", "a")])', 3);
+  });
+});
