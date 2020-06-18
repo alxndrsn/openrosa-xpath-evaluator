27e595ed20b34bafe3a6bc157472090431e90639
Alex Anderson <alxndrsn>
2020-06-18 15:25:05 +0000

    Add a couple more test cases

--- test/integration/predicates.spec.js
+++ test/integration/predicates.spec.js
@@ -10,10 +10,11 @@ describe.only('predicates with function calls', ()=> {
       [ '/data/item[2]/number',            assertNumberValue, 6 ],
       [ '/data/item[true()]/number + 1',   assertNumberValue, 5 ],
       [ '/data/item[true()]/number + 1',   assertStringValue, '5' ],
-      [ '/data/item[string-length("a") = 1]/number + 2',    assertNumberValue, 6 ],
-      [ '/data/item[2]/number + 3',                         assertNumberValue, 9 ],
-      [ '/data/item[string-length(./number)=1]/number + 3', assertNumberValue, 7 ],
-      [ '/data/item[(./number div 3.14) > 1.9]/number',     assertNumberValue, 6 ],
+      [ '/data/item[string-length("a") = 1]/number + 2',      assertNumberValue, 6 ],
+      [ '/data/item[2]/number + 3',                           assertNumberValue, 9 ],
+      [ '/data/item[string-length(./number)=1]/number + 3',   assertNumberValue, 7 ],
+      [ '/data/item[string-length(./number) = 1]/number + 3', assertNumberValue, 7 ],
+      [ '/data/item[(./number div 3.14) > 1.9]/number',       assertNumberValue, 6 ],
     ].forEach(([ expr, assertion, ...extraArgs ]) => {
       it(`should evaluate ${expr} as expected`, () => {
         initDoc(`
@@ -37,12 +38,14 @@ describe.only('predicates with function calls', ()=> {
 
   describe('with extended functions', () => {
     [
-      [ 'pi()',                                         assertNumberValue, 3.141592653589793 ],
-      [ '/data/item[1]/number',                         assertNumberValue, 4 ],
-      [ '/data/item[true()]/number',                    assertNumberValue, 4 ],
-      [ '/data/item[pi() > 3]/number',                  assertNumberValue, 4 ],
-      [ '/data/item[tan(./number) > 1]/number',         assertNumberValue, 4 ],
-      [ '/data/item[(./number div pi()) > 1.9]/number', assertNumberValue, 6 ],
+      [ 'pi()',                                          assertNumberValue, 3.141592653589793 ],
+      [ '/data/item[1]/number',                          assertNumberValue, 4 ],
+      [ '/data/item[true()]/number',                     assertNumberValue, 4 ],
+      [ '/data/item[pi() > 3]/number',                   assertNumberValue, 4 ],
+      [ '/data/item[tan(./number) > 1]/number',          assertNumberValue, 4 ],
+      [ '/data/item[tan(./number) <= 1]/number',         assertNumberValue, 6 ],
+      [ '/data/item[(./number div pi()) >  1.9]/number', assertNumberValue, 6 ],
+      [ '/data/item[(./number div pi()) <= 1.9]/number', assertNumberValue, 4 ],
     ].forEach(([ expr, assertion, ...extraArgs ]) => {
       it(`should evaluate ${expr} as expected`, () => {
         initDoc(`
